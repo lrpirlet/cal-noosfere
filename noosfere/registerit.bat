35bc6e51ee3dbf8e3d8f7cd8c16d305b1fe4e711
lrpirlet <lrpirlet@gmail.com>
2021-05-14 12:17:29 +0200

    added minor comments to clarify after questions

--- noosfere/registerit.bat
+++ noosfere/registerit.bat
@@ -1,1 +1,4 @@
-calibre-customize -b .
+rem see https://manual.calibre-ebook.com/generated/en/calibre-customize.html
+rem under *x, from a shell prompt, issue something like:  sudo calibre-customize -b . 
+
+calibre-customize -b . 
