a157eb826d4f8e387531bd4e05556e6a244d2cad
lrpirlet <lrpirlet@gmail.com>
2021-12-09 10:20:08 +0100

    remove excell file, added runit to kill calibre, register a plugin, start calibre again

---
+++ noosfere/runit.bat
@@ -0,0 +1,6 @@
+rem see https://manual.calibre-ebook.com/creating_plugins.html#id14 goto Debugging plugins...
+rem run runit from from the directory where the plugin is deveopped 
+
+calibre-debug -s
+calibre-customize -b .
+calibre
